95d46ee085d00617b7902ceff86f41d3b467325a
madman1397 <132142305+madman1397@users.noreply.github.com>
2023-12-01 10:04:20 +0000

    2023/Day_1 PART ONE COMPLETE

--- Shayne/Solutions/2023/Day_1/Python/Day_1.py
+++ Shayne/Solutions/2023/Day_1/Python/Day_1.py
@@ -1,1 +1,8 @@
-crudeInput = open("AdventOfCode\\Input\\Control\\2023\\Day_1.txt").readlines()
+#Define input, and strip data
+Control = [line.strip() for line in open(str('/'.join(__file__.split('/')[:3])+'/Input/Control/'+'/'.join(__file__.split('/')[(len(__file__.split('/'))-4):(len(__file__.split('/'))-2)])+'.txt')).readlines()]
+Unique = None
+Input = Control
+#Iterate data, remove non numeric characters, then concatenate only the first and last numerics on the line
+Nums = [int(i[0]+i[-1]) for i in [''.join(filter(str.isdigit, i)) for i in Input]]
+PartOne = sum(Nums)
+print(PartOne)
