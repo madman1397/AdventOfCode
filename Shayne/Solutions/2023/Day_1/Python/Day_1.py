1724c19e409e94da885cf77d6df6d5993f6e1673
madman1397 <madman1397@gmail.com>
2023-12-01 15:48:53 -0500

    2023/Day_1/Python/Day_1.py PART 2 COMPLETE
    Signed-off-by: madman1397 <madman1397@gmail.com>

--- Shayne/Solutions/2023/Day_1/Python/Day_1.py
+++ Shayne/Solutions/2023/Day_1/Python/Day_1.py
@@ -1,8 +1,47 @@
+import os
+import re
 #Define input, and strip data
-Control = [line.strip() for line in open(str('/'.join(__file__.split('/')[:3])+'/Input/Control/'+'/'.join(__file__.split('/')[(len(__file__.split('/'))-4):(len(__file__.split('/'))-2)])+'.txt')).readlines()]
-Unique = None
-Input = Control
-#Iterate data, remove non numeric characters, then concatenate only the first and last numerics on the line
-Nums = [int(i[0]+i[-1]) for i in [''.join(filter(str.isdigit, i)) for i in Input]]
-PartOne = sum(Nums)
+Type = 'Shayne'
+Input = [line.strip() for line in open(os.path.join(os.path.sep.join(__file__.split(os.path.sep)[:6]),'Input',Type,__file__.split(os.sep)[-4],(__file__.split(os.sep)[-1]).split('.')[0]+'.txt')).readlines()]
+
+#Iterate input, remove non numeric characters, then concatenate only the first and last numerics on the line
+def getNums(fInput):
+    Numerics = [''.join(filter(str.isdigit, i)) for i in fInput]
+    EndCaps = [int(i[0]+i[-1]) for i in Numerics]
+    return EndCaps
+
+PartOne = sum(getNums(Input))
 print(PartOne)
+
+#---------------PART-TWO-----------------#
+def Words2Nums(line):
+    #analyze line with a moving 'frame', if frame sees number, or verbose number, append to new list as int, then return that list for further handling
+    NumDict = {'zero':'0','one':'1','two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
+    numLine = []
+    minLen = len(min(NumDict, key=len))
+    maxLen = len(max(NumDict, key=len))
+    frameStart = 0
+    #Iteration Limiter: cease iteration when frame reaches end of line
+    while frameStart<len(line):
+        #Define the initial size of the view frame
+        frameEnd = frameStart+minLen
+        #If first index in frame is numeric, append to numLine, skip to next start index before iterating further
+        if line[frameStart].isnumeric():
+            numLine.append(line[frameStart])
+            frameStart += 1
+        #each iteration, move the frame over(up) one index, adjust frame width for each possible word size,-
+        #-each time checking for a match in NumDict. If match, append its corresponding int to numLine
+        else:
+            while frameEnd<=frameStart+maxLen:
+                frame = line[frameStart:frameEnd]
+                if frame in NumDict:
+                    numLine.append(NumDict[frame])              
+                frameEnd +=1
+            frameStart += 1
+    #return all numerics, verbose and int, in order of occurance, joined as one int (ex. seven3p5nine --> 7359)
+    return ''.join(numLine)
+        
+
+P2Input = [Words2Nums(line) for line in Input]
+PartTwo = sum(getNums(P2Input))
+print(PartTwo)
