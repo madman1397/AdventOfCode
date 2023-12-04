c3cbc9e538fd27434a640b43151a430d966009e0
Maxx115 <mka.gawletta@googlemail.com>
2023-12-04 09:05:05 +0100

    Max day 4 p1

--- Max/2023/Day_4/Python/Day_4.py
+++ Max/2023/Day_4/Python/Day_4.py
@@ -4,10 +4,44 @@ sys.path.insert(1, 'Setup')
 import input
 import io
 import unittest
-                
-if __name__ == '__main__':
-    pass
+
+def splitCard(strLine):
+
+    strLine = strLine.split(':')[1]
+    numList = strLine.split('|')
+    numList[0] = numList[0].split()
+    numList[1] = numList[1].split()
+
+    return numList
+
+def getWinning(numList):
+    retValue = 0
+
+    for draw in numList[0]:
+        for wNum in numList[1]:
+            if draw == wNum:
+                if retValue == 0: retValue += 1
+                else: retValue *= 2
     
+    return retValue
+
+def collectWinnings(strlist):
+    retValue = 0
+
+    for line in strlist:
+        numList = splitCard(line)
+        retValue += getWinning(numList)
+
+    return retValue
+
+if __name__ == '__main__':
+    control = input.getControlInput('2023', 'Day_4')
+    print('---CONTROL P1---')
+    print(collectWinnings(control))
+
+    puzzle = input.getPuzzleInput('2023', 'Day_4')
+    print('---PUZZLE P1---')
+    print(collectWinnings(puzzle))   
                 
 ################################################################
 class TestStringMethods(unittest.TestCase):
@@ -15,7 +49,21 @@ class TestStringMethods(unittest.TestCase):
     def test_control_file(self):
         control = input.getControlInput('2023', 'Day_4')
         self.assertEqual(True, bool(control) and all(isinstance(elem, str) for elem in control))
+        print(control)
     def test_puzzle_file(self):
         puzzle = input.getPuzzleInput('2023', 'Day_4')
         self.assertEqual(True, bool(puzzle) and all(isinstance(elem, str) for elem in puzzle))
-                
+    
+    def test_splitCard(self):
+        strInput ='Card   1: 12 34 44 | 12 33 98\n'
+        self.assertEqual([['12', '34', '44'], ['12', '33', '98']], splitCard(strInput))
+
+    def test_getWinning(self):
+        listInput = [['12', '34', '44'], ['12', '33', '98']]
+        self.assertEqual(1, getWinning(listInput))
+        listInput = [['12', '34', '44', '55'], ['12', '34', '44', '55']]
+        self.assertEqual(8, getWinning(listInput))
+
+    def test_collectWinnings(self):
+        control = ['Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n', 'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n', 'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n', 'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n', 'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n', 'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11']
+        self.assertEqual(13, collectWinnings(control))
