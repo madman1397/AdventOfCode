b7035b209623c4208b522a6e4587ac6b11a6be20
Maxx115 <30586848+Maxx115@users.noreply.github.com>
2023-12-19 15:15:22 +0000

    Day 8 not done but far

--- Max/2023/Day_8/Python/Day_8.py
+++ Max/2023/Day_8/Python/Day_8.py
@@ -41,6 +41,39 @@ def solution(listInput):
             index = (index + 1) % dirLen
     return countJumps
 
+def getAllCurrentPos(dictRoutes):
+    startPos = [key for key in dictRoutes.keys() if key.endswith('A')]
+    return startPos
+
+def getNextPos(dictRoutes, currentPos, direction):
+    nextPos = [dictRoutes[key][direction] for key in currentPos if key in dictRoutes]
+    return nextPos
+
+def checkEnd(currentPos):
+    end = all(s.endswith('Z') for s in currentPos)
+    return end
+
+def solution2(listInput):
+    directions = getDirection(listInput[0])
+    routes = getRoutesDict(listInput[2:])
+    countJumps = 0
+    currentPos = getAllCurrentPos(routes)
+    index = 0
+    dirLen = len(directions)
+
+    #print(currentPos)
+
+    while True:
+        nextPos = getNextPos(routes, currentPos, directions[index])
+        currentPos = nextPos
+        #print(currentPos)
+        countJumps += 1
+        if checkEnd(currentPos):
+            break
+        else:
+            index = (index + 1) % dirLen
+    return countJumps
+
 if __name__ == '__main__':
     control = input.getControlInput('2023', 'Day_8')
     countJumps = solution(control)
@@ -49,6 +82,13 @@ if __name__ == '__main__':
     puzzle = input.getPuzzleInput('2023', 'Day_8')
     countJumps = solution(puzzle)
     print('Puzzle =',countJumps)
+
+    control = input.getControlInput('2023', 'Day_8_P2')
+    countJumps = solution2(control)
+    print('Control P2:', countJumps)
+
+    countJumps = solution2(puzzle)
+    print('Puzzle P2:', countJumps)
                 
 ################################################################
 class TestStringMethods(unittest.TestCase):
@@ -67,3 +107,22 @@ class TestStringMethods(unittest.TestCase):
     def test_getRoutesDict(self):
         listInput = ['AAA = (BBB, BBB)\n', 'BBB = (AAA, ZZZ)\n', 'ZZZ = (ZZZ, ZZZ)']
         self.assertEqual({'AAA':['BBB', 'BBB'], 'BBB':['AAA', 'ZZZ'], 'ZZZ':['ZZZ', 'ZZZ']}, getRoutesDict(listInput))
+    
+    def test_getAllCurrentPos(self):
+        control = input.getControlInput('2023', 'Day_8_P2')
+        dictRoutes = getRoutesDict(control[2:])
+        self.assertEqual(['11A', '22A'] ,getAllCurrentPos(dictRoutes))
+    
+    def test_getNetPos(self):
+        control = input.getControlInput('2023', 'Day_8_P2')
+        dictRoutes = getRoutesDict(control[2:])
+        currentPos = getAllCurrentPos(dictRoutes)
+        direction = 0
+        nextPos = getNextPos(dictRoutes, currentPos, direction)
+        self.assertEqual(['11B', '22B'], nextPos)
+
+    def test_checkEnd(self):
+        currentPos = ['11Z', '22V']
+        self.assertEqual(False, checkEnd(currentPos))
+        currentPos = ['11Z', '22Z']
+        self.assertEqual(True, checkEnd(currentPos))
